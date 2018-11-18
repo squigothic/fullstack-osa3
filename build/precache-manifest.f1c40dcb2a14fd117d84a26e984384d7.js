797563cd8c15f63560e48591db68ef09a43d4141
Teemu Laitila <squigothic@gmail.com>
2018-11-18 20:59:08 +0200

    frontend siirretty samaan repositorioon

---
+++ build/precache-manifest.f1c40dcb2a14fd117d84a26e984384d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c414d0c575841608c007",
+    "url": "/static/js/main.c414d0c5.chunk.js"
+  },
+  {
+    "revision": "b08413150464f681f98f",
+    "url": "/static/js/1.b0841315.chunk.js"
+  },
+  {
+    "revision": "c414d0c575841608c007",
+    "url": "/static/css/main.8952261b.chunk.css"
+  },
+  {
+    "revision": "b170fe09ae629fce6652f240ce463622",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
