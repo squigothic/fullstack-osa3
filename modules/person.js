b410f279152e83576f13b47edd97180dd4c2d224
Teemu Laitila <squigothic@gmail.com>
2018-12-04 22:14:52 +0200

    added env variables for deployment

--- modules/person.js
+++ modules/person.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
 
 const url = process.env.DB_HOST
 
